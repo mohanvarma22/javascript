4fe3ad83eeb829cb8d94cd9a9b6e1b23ab63a16c
Mohan <mohanvarma1022@gmail.com>
2024-10-23 18:37:55 +0530

    more about datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,8 +2,8 @@
 
 // alert("hello") cannot be used in nodejs, use browser
 
-console.log(5)
-console.log("mohan")
+// console.log(5)
+// console.log("mohan")
 
 //read documentation from mdn
 
@@ -16,7 +16,33 @@ let isLoggedIn=false//boolean
 //symbol => unique
 
 //object
-console.log(typeof null)
+// console.log(typeof null)
 
 // null type object
 
+//primitive  data type call by value
+// 7 types: String,number,boolean,null,symbol,bigint
+
+//non primitive reference type
+// Arrays,objects,Functions
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id===anotherId);
+// console.log(id)
+// console.log(anotherId)
+
+const heros =["mohan",'varma']//arrays
+
+// let myObj = {
+//     name:"mohan",
+//     age:21
+// }
+
+// console.log(myObj)
+
+const myFunction = function(){
+    console.log("Hi Guysss")
+}
+
+myFunction()
