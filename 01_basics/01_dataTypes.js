c7d882194999c0ce57f6fa91eda6f3465c67034e
Mohan <mohanvarma1022@gmail.com>
2024-10-23 17:10:52 +0530

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") cannot be used in nodejs, use browser
+
+console.log(5)
+console.log("mohan")
+
+//read documentation from mdn
+
+let name ="mohan"//string
+let age=18 //int
+let isLoggedIn=false//boolean
+//bigint if the number is tooo big
+//null => standalone value
+//undefined 
+//symbol => unique
+
+//object
+console.log(typeof null)
+
+// null type object
+
