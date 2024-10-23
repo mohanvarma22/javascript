8c6ff7ab5ec89db8400147b0c6159626e3cad7e0
Mohan <mohanvarma1022@gmail.com>
2024-10-23 13:50:24 +0530

    learned about variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 21007 //cannot be changed
+let accountEmail = "mohan@gmail.com"
+var accountPassword = "123"
+accountCity="hyderabad"//not a good practice
+
+console.log(accountEmail);
+
+console.table([accountEmail,accountId])
+
+//diff between var and let, we only use let because we can avoid scope issue
+
+/*  dont use var */
+
