471984bbaac389ae84c8060b35c610d76cecbb56
Mohan <mohanvarma1022@gmail.com>
2024-10-23 17:36:46 +0530

    datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,13 +6,13 @@
 
 // console.log(typeof valueInNumber)
 
-let score = "22abc"
+// let score = "22abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
 console.log(valueInNumber)//proper number ivvakpothe NaN ochiddi
 //null => 0 ; undefined => NaN ; boolean ayte 0 or 1;
@@ -20,3 +20,14 @@ console.log(valueInNumber)//proper number ivvakpothe NaN ochiddi
 //1=> boolean() => True
 
 //name or anythig => true empty ayte false
+
+// let value =3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hi";
+// let str2=" mohan"
+// let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1"+2)//string starting lo unte mothanne string la treat chestaru 
