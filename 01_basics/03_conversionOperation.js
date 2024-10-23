d26688122170ad9c0fedbf308d571100365606cd
Mohan <mohanvarma1022@gmail.com>
2024-10-23 17:23:33 +0530

    convertionoperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = "22"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+
+let score = "22abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)//proper number ivvakpothe NaN ochiddi
+//null => 0 ; undefined => NaN ; boolean ayte 0 or 1;
+
+//1=> boolean() => True
+
+//name or anythig => true empty ayte false
