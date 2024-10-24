6edbd1c25aa42610f1deca7163464196a781193d
Mohan <mohanvarma1022@gmail.com>
2024-10-24 22:41:49 +0530

    basic functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+
+
+function sayMyName(){
+    console.log("mohan");
+    console.log("varma");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2)
+// }
+
+function addTwoNumbers(number1,number2){
+    let result = number1 + number2
+    return result
+
+}
+
+const result = addTwoNumbers(2,3) 
+console.log("rsult",result)
