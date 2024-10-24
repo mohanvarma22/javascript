db4ffe558cf702c537df4e333e4e08ab322f76a5
Mohan <mohanvarma1022@gmail.com>
2024-10-24 11:30:46 +0530

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "mohan"
+const repoCount = 2
+// console.log(name+repoCount) this not the ideal syntax instead use the below one
+
+console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);// string interpolation
+
+const Name =  new String('Mohanvarma')
+
+console.log(Name[5]);
+console.log(Name.length)
+
+const string = "  mohwn"
+console.log(string.trim())
+
+console.log(string.replace('moh','-'))
+//split,slice ila chala untay refer mdn documentation
