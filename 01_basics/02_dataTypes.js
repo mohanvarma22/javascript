d2eb2eee6961ffa4f981516be58f55875eee88ba
Mohan <mohanvarma1022@gmail.com>
2024-10-24 11:02:46 +0530

    stack and heap

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -32,7 +32,7 @@ let isLoggedIn=false//boolean
 // console.log(id)
 // console.log(anotherId)
 
-const heros =["mohan",'varma']//arrays
+// const heros =["mohan",'varma']//arrays
 
 // let myObj = {
 //     name:"mohan",
@@ -41,8 +41,35 @@ const heros =["mohan",'varma']//arrays
 
 // console.log(myObj)
 
-const myFunction = function(){
-    console.log("Hi Guysss")
-}
+// const myFunction = function(){
+//     console.log("Hi Guysss")
+// }
 
-myFunction()
+// myFunction()
+
+//stack(primitive)  in primitve only the copy is shared
+//heap(non-primitive) in non primitive original value's reference is shared
+
+// let s = "mohan"
+// let m = s
+// m = "Mohan"
+// console.log(s);
+// console.log(m)
+
+// let user = {
+//     email: "user@google.com",
+//     upi: "user@ibl"
+// }
+
+// let user2 = user
+
+// user2.email="mohanvarma"
+
+// console.log(user);
+// console.log(user2)
+
+/*as you can see here the value of user 
+is also changed when you change the value pf user2 */
+
+
+
