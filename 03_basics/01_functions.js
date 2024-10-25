aeede40704ec460a06e5fd45a4f4929d1b4b0ebd
Mohan <mohanvarma1022@gmail.com>
2024-10-25 18:16:42 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,4 +18,27 @@ function addTwoNumbers(number1,number2){
 }
 
 const result = addTwoNumbers(2,3) 
-console.log("rsult",result)
+// console.log("rsult",result)
+
+function calculateCart(...num1){
+    return num1
+}
+
+// console.log(calculateCart(200,200,304,234))
+
+const user ={
+    username: "mohan"
+}
+
+function handleObject(anyobject){
+    // console.log(`username is ${anyobject.username}`)
+}
+
+handleObject(user)
+
+function returnS(arr){
+    return arr[1]
+}
+
+console.log(returnS([4,6,7]))
+
