2e6129b7d0d86b723c9ea19a36585c2de7b30193
Mohan <mohanvarma1022@gmail.com>
2024-10-26 22:07:20 +0530

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+//immediately inovoked function expressions(IIFE)
+
+// to prevent global scope pollution iife is used 
+
+// (define a function)(to call the function)
+
+(function biryani(){
+    console.log('connected');
+})();//semi colan pettadam marchpote end avvadu 
+
+(  (name) => {
+    console.log(`${name}`)
+}  )('mohan')
