ed88cd77db2592835f597fa676b1aff15058349d
Mohan <mohanvarma1022@gmail.com>
2024-10-26 23:02:23 +0530

    executioncontext

---
+++ 03_basics/executioncontext.js
@@ -0,0 +1,10 @@
+// global execution context browser lo this window undiddi 
+// gets executed in thread, js is single threaded
+//functional exec context(fec) 
+//eval exec context(not that imp)
+
+//two phases memory creation and execution
+
+//timestamp 6hr 15min revise well
+
+//stack execution
