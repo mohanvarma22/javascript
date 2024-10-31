6485a97d081f18fdd7a5386191fc11505297bc91
Mohan <mohanvarma1022@gmail.com>
2024-10-31 21:38:32 +0530

    Promises

--- 09_promises/promises.js
+++ 09_promises/promises.js
@@ -69,15 +69,21 @@
 //     }
 // }
 
-async function getAllusers(){
-    try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await response.json()
-        console.log(data);
+// async function getAllusers(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
 
-    }catch(error){
-        console.log(error)
-    }
-}
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
 
-getAllusers()
+// getAllusers()
+
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
+    return response.json()
+}).then((data) => {
+    console.log(data)
+}).catch((error)=>console.log(error))
